fa450e71a8026925aeffd2069b99233a80acfb4e
QuintenDes <quintendeswaef@gmail.com>
2021-12-09 12:51:31 +0100

    add licenses

--- contracts/HumanDaoDistributor.sol
+++ contracts/HumanDaoDistributor.sol
@@ -1,9 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
 import "./merkle/MerkleDistributor.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract HumanDaoDistributor is MerkleDistributor {
 
-    constructor(address token_, bytes32 merkleRoot_) public MerkleDistributor(token_, merkleRoot_) {
+    constructor(address token_, bytes32 merkleRoot_) MerkleDistributor(token_, merkleRoot_) {
         //intentionally left empty
     }
 
