f314fee8c2125cb719d2359da92bf682fd361e0d
QuintenDes <quintendeswaef@gmail.com>
2021-12-10 11:01:02 +0100

    Fix contract, start of test harness

---
+++ test/Distributions.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+
+describe('Distribution Contract', function () {
+
+    it('should be possible to verify a proof', async () => {
+
+        const [owner] = await ethers.getSigners();
+
+
+    });
+
+});
