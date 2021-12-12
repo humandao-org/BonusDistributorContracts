81e84775b87174a0d6cdbec17e73dde14dbe1928
Barbarian Finance <95499220+barbarian-fi@users.noreply.github.com>
2021-12-12 11:01:50 -0500

    Update HumanDaoGenesisNFT.sol

--- contracts/HumanDaoGenesisNFT.sol
+++ contracts/HumanDaoGenesisNFT.sol
@@ -3,23 +3,28 @@ pragma solidity ^0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
+// import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract HumanDaoGenesisNFT is ERC721, Ownable {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "http://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-    string public baseUri = "";
+contract HumanDaoGenesisNFT is ERC721URIStorage, Ownable  {
+
+    uint256 private _totalSupply;
+    string private _tokenURI = "ipfs://QmdMQdtA8Xju3eM9SXt9enPpNEgpQqZrta1WGYYerAVwXe";
+    function totalSupply() public view virtual returns (uint256) {
+        return _totalSupply;
+    }
 
     constructor() ERC721('HumanDAOGenesis', 'HDAOGEN') {}
 
-    function mint(address beneficiary, uint256 tokenId) external onlyOwner {
+    function mint(address beneficiary, uint tokenId) external onlyOwner {
         _mint(beneficiary, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        _totalSupply += 1;
     }
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseUri;
-    }
-
-    function setBaseURI(string calldata baseUri_) external onlyOwner{
-        baseUri = baseUri_;
-    }
 }
